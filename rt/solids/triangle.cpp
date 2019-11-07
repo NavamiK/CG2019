306296b155b48c90207e4a44d3f8fc4f377324d9
s8nakair <s8nakair@stud.uni-saarland.de>
2019-11-07 06:57:17 +0100

    file missing

--- rt/solids/triangle.cpp
+++ rt/solids/triangle.cpp
@@ -5,11 +5,17 @@ namespace rt {
 Triangle::Triangle(Point vertices[3], CoordMapper* texMapper, Material* material)
 {
     /* TODO */
+    this->vertices[0] = vertices[0];
+    this->vertices[0] = vertices[0];
+    this->vertices[0] = vertices[0];
 }
 
 Triangle::Triangle(const Point& v1, const Point& v2, const Point& v3, CoordMapper* texMapper, Material* material)
 {
     /* TODO */
+    this->v1 = v1;
+    this->v2 = v2;
+    this->v3 = v3;
 }
 
 BBox Triangle::getBounds() const {
@@ -17,7 +23,37 @@ BBox Triangle::getBounds() const {
 }
 
 Intersection Triangle::intersect(const Ray& ray, float previousBestDistance) const {
-    /* TODO */ NOT_IMPLEMENTED;
+    /* TODO */
+    const float EPSILON = 0.0000001;
+    Vector edge1, edge2, h, s, q;
+    Vector rayVector = ray.d;
+    Point rayOrigin = ray.o;
+    float a,f,u,v;
+    edge1 = v2 - v1;
+    edge2 = v3 - v1;
+    h = cross(rayVector, edge2);
+    a = dot(edge1, h);
+    if (a > -EPSILON && a < EPSILON)
+        return Intersection::failure();   // This ray is parallel to this triangle.
+    f = 1.0/a;
+    s = rayOrigin - v1;
+    u = f * dot(s, h);
+    if (u < 0.0 || u > 1.0)
+        return Intersection::failure();
+    q = cross(s, edge1);
+    v = f * dot(rayVector, q);
+    if (v < 0.0 || u + v > 1.0)
+       return Intersection::failure();
+    // At this stage we can compute t to find out where the intersection point is on the line.
+    float t = f * dot(edge2, q);
+    if ((t > EPSILON && t < 1/EPSILON)&& (t < previousBestDistance)) // ray intersection
+    {
+        Intersection intersection(t, ray, this, ray.d.normalize(), ray.getPoint(t));
+        return intersection;
+    }
+    else // This means that there is a line intersection but not a ray intersection.
+        return  Intersection::failure();;
+
 }
 
 Solid::Sample Triangle::sample() const {
