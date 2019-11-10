518e3e265c6388a717d0c8d384d1ddb9fc40d01f
s8nakair <s8nakair@stud.uni-saarland.de>
2019-11-10 11:32:54 +0100

    BBox empty implemented

--- rt/bbox.cpp
+++ rt/bbox.cpp
@@ -3,7 +3,8 @@
 namespace rt {
 
 BBox BBox::empty() {
-    /* TODO */ NOT_IMPLEMENTED;
+    /* TODO */ 
+    return BBox();
 }
 
 BBox BBox::full() {
@@ -13,25 +14,34 @@ BBox BBox::full() {
 
 void BBox::extend(const Point& point) {
     /* TODO */    
+    if(this->isEmpty)
+        min = max = point;
+    else{
+        min.x = std::min(point.x, min.x);
+        min.y = std::min(point.y, min.y);    
+        min.z = std::min(point.z, min.z);
 
-    min.x = std::min(point.x, min.x);
-    min.y = std::min(point.y, min.y);    
-    min.z = std::min(point.z, min.z);
-
-    max.x = std::max(point.x, max.x);
-    max.y = std::max(point.y, max.y);
-    max.z = std::max(point.z, max.z);
+        max.x = std::max(point.x, max.x);
+        max.y = std::max(point.y, max.y);
+        max.z = std::max(point.z, max.z);
+    }
 }
 
 void BBox::extend(const BBox& bbox) {
     /* TODO */ 
-    min.x = std::min(bbox.min.x, min.x);
-    min.y = std::min(bbox.min.y, min.y);    
-    min.z = std::min(bbox.min.z, min.z);
+    if(this->isEmpty){
+        min = bbox.min;
+        max = bbox.max;
+    }
+    else{
+        min.x = std::min(bbox.min.x, min.x);
+        min.y = std::min(bbox.min.y, min.y);    
+        min.z = std::min(bbox.min.z, min.z);
 
-    max.x = std::max(bbox.max.x, max.x);
-    max.y = std::max(bbox.max.y, max.y);
-    max.z = std::max(bbox.max.z, max.z);
+        max.x = std::max(bbox.max.x, max.x);
+        max.y = std::max(bbox.max.y, max.y);
+        max.z = std::max(bbox.max.z, max.z);
+    }
 }
 
 std::pair<float, float> BBox::intersect(const Ray& ray) const {
