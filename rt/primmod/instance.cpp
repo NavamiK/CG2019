d38bcef6472665b34870673c39f680c4b8dfb03d
ebrima <ebrimamaubeh7@gmail.com>
2019-11-22 00:13:57 +0100

    changed rotation

--- rt/primmod/instance.cpp
+++ rt/primmod/instance.cpp
@@ -31,25 +31,46 @@ void Instance::translate(const Vector& t) {
 
 void Instance::rotate(const Vector& nnaxis, float angle) {
     /* TODO */
-    Matrix tMatrix, m1, m2;
+    //1. translate p to origin (what's p)?
+    //2. translate m
+    //3. rotate around x
+    //4. transform back with mt
+    //5. translate back to p.
+
+    //////////////// crate a system of matrices.
     float x = nnaxis.x, y = nnaxis.y, z = nnaxis.z;
-    m1 = {
-            Float4(x*x, x*y, x*z, 0.f),
-            Float4(x*y, y*y, y*z, 0.f),
-            Float4(x*z, y*z, z*z, 0.f),
-            Float4(0.f, 0.f, 0.f, 1.f)
+    Vector s = Vector::rep(0.f);
+    float minVal = min(min(x, y), z);
+    if(x == minVal){
+        s = Vector(0, -z, y);
+    }
+    if(y == minVal){
+        s = Vector(-z, 0, x);
+    }
+    if(z == minVal){
+        s = Vector(-y, x, 0);
+    }
+    s = s.normalize();
+
+
+    //failed.
+    assert(s != Vector::rep(0.f));
+    assert(dot(s, nnaxis) == 0); // must be orthogonal.
+
+    Vector t = cross(nnaxis, s);
+    Vector e1 = nnaxis, e2 = s, e3 = t;
+    Matrix m = Matrix::system(e1, e2, e3);
+    Matrix mt = m.invert();
+    float B = (pi/180.f) * angle;
+    //////////////// crate a system of matrices.
+    Matrix Rx{
+        Float4(1.f, 0.f    , 0.f      ,0.f),
+        Float4(0.f, cos(B) , -sin(B)  ,0.f),
+        Float4(0.f, sin(B), cos(B)  ,0.f),
+        Float4(0.f, 0.f    , 0.f     ,1.f)
     };
 
-    m2 = {
-            Float4( 0,    -z,   y,   0.f),
-            Float4( z,     0,   -x,   0.f),
-            Float4(-y,    x,     0,   0.f),
-            Float4(0.f, 0.f,   0.f,   1.f)
-    };
-
-    float B = (pi / 180.f) * angle;//convert to radians.
-    tMatrix = cos(B)*(Matrix::identity()) + (1.f - cos(B))*m1 + sin(B)*m2;
-    transformation = product(tMatrix, transformation);
+    transformation = product(product(m, Rx), mt); // the formula on slide 30, missing point tho.
 }
 
 void Instance::scale(float f) {
