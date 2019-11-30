7c03cb04275f53c5372d85a20a841fb122557961
s8nakair <s8nakair@stud.uni-saarland.de>
2019-11-30 19:19:45 +0100

    Offset for shadow ray

--- rt/integrators/raytrace.cpp
+++ rt/integrators/raytrace.cpp
@@ -4,6 +4,7 @@
 #include <rt/solids/solid.h>
 #include <rt/materials/material.h>
 
+float OFFSET = 0.0001f;
 namespace rt {
 
 RGBColor RayTracingIntegrator::getRadiance(const Ray& ray) const {
@@ -14,23 +15,21 @@ RGBColor RayTracingIntegrator::getRadiance(const Ray& ray) const {
     if(intersection){
         for(int i = 0; i < world->light.size(); i++){
             LightHit lightHit = world->light[i]->getLightHit(intersection.hitPoint());
-            Ray shadowRay(intersection.hitPoint(), lightHit.direction);
-            if(dot(intersection.normal(), shadowRay.d) > 0){
-                Intersection shaIntersec = world->scene->intersect(shadowRay);
+            //Shift the ray origin towards it's direction by an offset, to avoid self intersection
+            Ray shadowRay(intersection.hitPoint() + lightHit.direction * OFFSET, lightHit.direction);
+            if(dot(intersection.normal(), shadowRay.d) > 0.0f){
+                Intersection shaIntersec = world->scene->intersect(shadowRay, lightHit.distance);
                 //If no intersection of shadow ray, or the intersection distnace greater than distance to light source, update radiance
-                if((!shaIntersec) || (shaIntersec.distance >= lightHit.distance)){
+                if(!shaIntersec){
                     intensity = world->light[i]->getIntensity(lightHit);
                     emission = intersection.solid->material->getEmission(intersection.local(), intersection.normal(), -ray.d);
-                    reflectance = intersection.solid->material->getReflectance(intersection.local(), intersection.normal(),  -ray.d, -lightHit.direction);
-                    //totalRadiance = totalRadiance + emission + intensity * reflectance;
-                    totalRadiance = totalRadiance + emission + intensity;
+                    reflectance = intersection.solid->material->getReflectance(intersection.local(), intersection.normal(),  -ray.d, -shadowRay.d);
+                    totalRadiance = totalRadiance + emission + intensity * reflectance;
                 }
             }
-        }
-        return totalRadiance;
+        }   
     }
-    else
-        return RGBColor::rep(0.0f);   
+    return totalRadiance;
 }
 
 }
