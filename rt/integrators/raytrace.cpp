fa29dc411ab56cdce01720a94230baa14cc44de5
s8nakair <s8nakair@stud.uni-saarland.de>
2019-12-08 09:50:12 +0100

    Corrections from last assignment

--- rt/integrators/raytrace.cpp
+++ rt/integrators/raytrace.cpp
@@ -13,19 +13,20 @@ RGBColor RayTracingIntegrator::getRadiance(const Ray& ray) const {
     RGBColor emission, reflectance, intensity;
     Intersection intersection = world->scene->intersect(ray);
     if(intersection){
+        emission = intersection.solid->material->getEmission(intersection.local(), intersection.normal(), -ray.d);
+        totalRadiance = totalRadiance + emission;
         for(int i = 0; i < world->light.size(); i++){
             LightHit lightHit = world->light[i]->getLightHit(intersection.hitPoint());
             //Shift the ray origin towards it's direction by an offset, to avoid self intersection
-            Ray shadowRay(intersection.hitPoint() + lightHit.direction * OFFSET, lightHit.direction);
+            Ray shadowRay(intersection.hitPoint() +intersection.normal() * OFFSET, lightHit.direction);
             if(dot(intersection.normal(), shadowRay.d) > 0.0f){
                 Intersection shaIntersec = world->scene->intersect(shadowRay, lightHit.distance);
                 //If no intersection of shadow ray, or the intersection distnace greater than distance to light source, update radiance
                 if(!shaIntersec){
                     intensity = world->light[i]->getIntensity(lightHit);
                     Vector t = cross(lightHit.direction,intersection.normal());
-                    emission = intersection.solid->material->getEmission(intersection.local(), intersection.normal(), -ray.d);
                     reflectance = intersection.solid->material->getReflectance(intersection.local(), intersection.normal(),  -ray.d, -shadowRay.d);
-                    totalRadiance = totalRadiance + emission + intensity * reflectance;
+                    totalRadiance = totalRadiance + intensity * reflectance;
                 }
             }
         }   
