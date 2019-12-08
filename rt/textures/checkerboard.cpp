15456f61c88f7d523fc7c508066800f93aa39a94
s8nakair <s8nakair@stud.uni-saarland.de>
2019-12-08 18:31:16 +0100

    Perlin texture added

--- rt/textures/checkerboard.cpp
+++ rt/textures/checkerboard.cpp
@@ -1,5 +1,5 @@
 #include <rt/textures/checkerboard.h>
-
+#include <cmath>
 namespace rt {
 
 CheckerboardTexture::CheckerboardTexture(const RGBColor& white, const RGBColor& black)
@@ -12,14 +12,14 @@ CheckerboardTexture::CheckerboardTexture(const RGBColor& white, const RGBColor& 
 RGBColor CheckerboardTexture::getColor(const Point& coord) {
     /* TODO */
     /* Multiply by 2 to reduce edge length from 1 to 0.5 */
-    int lu = coord.x * 2;
-    int lv = coord.y * 2;
-    int lw = coord.z * 2;
+    int lu = floor(coord.x * 2.0f);
+    int lv = floor(coord.y * 2.0f);
+    int lw = floor(coord.z * 2.0f);
     int parity = (lu + lv + lw) % 2;
     if(parity == 0)
-        return black;
-    else 
         return white;
+    else 
+        return black;
 }
 
 RGBColor CheckerboardTexture::getColorDX(const Point& coord) {
