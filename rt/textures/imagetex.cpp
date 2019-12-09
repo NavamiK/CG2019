e6c83416ea7912514cb11bfbb0d6fea1e42ada26
s8nakair <s8nakair@stud.uni-saarland.de>
2019-12-09 20:58:28 +0100

    Parts of ImageTexture,  Lambertian material tested

--- rt/textures/imagetex.cpp
+++ rt/textures/imagetex.cpp
@@ -5,20 +5,31 @@ namespace rt {
 ImageTexture::ImageTexture()
 {
     /* TODO */
+    this->bh = REPEAT;
+    this->ip = BILINEAR;
 }
 
 ImageTexture::ImageTexture(const std::string& filename, BorderHandlingType bh, InterpolationType i)
 {
     /* TODO */
+    this->bh = bh;
+    this->ip = i;
+    Image image(512,512);
+    image.readPNG(filename);
+    this->img = image;
 }
 
 ImageTexture::ImageTexture(const Image& image, BorderHandlingType bh, InterpolationType i)
 {
     /* TODO */
+    this->bh = bh;
+    this->ip = i;
+    this->img = image;
 }
 
 RGBColor ImageTexture::getColor(const Point& coord) {
-    /* TODO */ NOT_IMPLEMENTED;
+    /* TODO */ 
+    NOT_IMPLEMENTED;
 }
 
 RGBColor ImageTexture::getColorDX(const Point& coord) {
