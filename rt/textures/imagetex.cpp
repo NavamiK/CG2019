fe10b394b6b5fdf2bce011ba13e16e972dfd7770
s8nakair <s8nakair@stud.uni-saarland.de>
2019-12-13 11:01:05 +0100

    ImageTextures fixed

--- rt/textures/imagetex.cpp
+++ rt/textures/imagetex.cpp
@@ -32,102 +32,148 @@ RGBColor ImageTexture::getColor(const Point& coord) {
     /* Implemented as per lecture notes, doesn't work for given image */
     float tu, tv, fu, fv, u, v, tmpu, tmpv;
     int lu, lv;
-    tu = coord.x;
-    tv = coord.y;
+    //tu = coord.x;
+    //tv = coord.y;
     //assert(tu>=0 && tv>=0);
     switch(ip){
         case NEAREST:
-            tmpu = tu * (img.width());
-            tmpv = tv * (img.height());
+            tmpu = coord.x * (img.width());
+            tmpv = coord.y * (img.height());
             switch (bh)
             {
             case CLAMP:               
-                if(tmpu<0) 
-                    tu = 0;
-                else if(tmpu>1)
-                    tu = 1;
+                if(tmpu<0.0f) 
+                    tu = 0.0f;
+                else if(tmpu>img.width()-1)
+                    tu = img.width()-1;
                 else tu = tmpu;
 
-                if(tmpv<0) 
-                    tv = 0;
-                else if(tmpv>1)
-                    tv = 1;
+                if(tmpv<0.f) 
+                    tv = 0.f;
+                else if(tmpv>img.height()-1)
+                    tv = img.height()-1;
                 else tv = tmpv;
                 break;
             case REPEAT:
-                tu = tmpu - (int)tmpu;
-                tv = tmpv - (int)tmpv;
+                if(tmpu<0.0f) 
+                    tu = img.width() + fmod(tmpu, img.width());
+                else if(tmpu>img.width()-1)
+                    tu = fmod(tmpu, img.width());
+                else tu = tmpu;
+
+                if(tmpv<0.f) 
+                    tv = img.height() + fmod(tmpv, img.height());
+                else if(tmpv>img.height()-1)
+                    tv = fmod(tmpv, img.height());
+                else tv = tmpv;
                 break;
             case MIRROR:
-                tu = tmpu - (int)tmpu;
-                tv = tmpv - (int)tmpv;
-                lu = (int)tmpu;
-                lv = (int)tmpv;
-                if(lu %2==1)
-                   tu = 1 - tu;
-                if(lv %2==1)
-                   tv = 1 - tv;
+                if(tmpu<0.0f) 
+                    tu = img.width() + fmod(tmpu, img.width());
+                else if(tmpu>img.width()-1)
+                    tu = fmod(tmpu, img.width());
+                    //tu = img.width() - fmod(tmpu, img.width());
+                else tu = tmpu;
+
+                if(tmpv<0.f) 
+                    tv = img.height() + fmod(tmpv, img.height());
+                else if(tmpv>img.height()-1)
+                    //tv = img.height() - fmod(tmpv, img.height());
+                    tv = fmod(tmpv, img.height());
+                else tv = tmpv;
+                lu = tmpu / img.width();
+                if(tmpu < 0)
+                    lu = lu + 1;
+                lv = tmpv / img.height();
+                if(tmpv < 0)
+                    lv = lv + 1;
+                if(lu%2==1)
+                    tu = img.width() - tu;
+                if(lv%2==1)
+                    tv = img.height() - tv;
                 break;
             }
-            u = tu * (img.width());
-            v = tv * (img.height());    
-            lu = min((int)u, (int)img.width()-1);
-            lv = min((int)v, (int)img.height()-1);
-             //if(lu>511 ||lv>511 )
-               // std::cout<<" lu no ";
-            //return img(1,1);
-            return img(lu, lv);
+            assert(((int)tu)<=511 && ((int)tv)<=511);
+            return img(tu, tv);
             break;
         case BILINEAR:
-            tmpu = tu * (img.width()-1);
-            tmpv = tv * (img.height()-1);
-            switch (bh){
+            tmpu = coord.x * (img.width()-1);
+            tmpv = coord.y * (img.height()-1);
+            switch (bh)
+            {
             case CLAMP:               
-                if(tmpu<0) 
-                    tu = 0;
-                else if(tmpu>1)
-                    tu = 1;
+                if(tmpu<0.0f) 
+                    tu = 0.0f;
+                else if(tmpu>img.width()-2)
+                    tu = img.width()-2;
                 else tu = tmpu;
 
-                if(tmpv<0) 
-                    tv = 0;
-                else if(tmpv>1)
-                    tv = 1;
+                if(tmpv<0.f) 
+                    tv = 0.f;
+                else if(tmpv>img.height()-2)
+                    tv = img.height()-2;
                 else tv = tmpv;
                 break;
             case REPEAT:
-                tu = tmpu - (int)tmpu;
-                tv = tmpv - (int)tmpv;
+                if(tmpu<0.0f) 
+                    tu = img.width() -1 + fmod(tmpu, img.width());
+                else if(tmpu>img.width()-2)
+                    tu = fmod(tmpu, img.width());
+                else tu = tmpu;
+
+                if(tmpv<0.f) 
+                    tv = img.height() -1 + fmod(tmpv, img.height());
+                else if(tmpv>img.height()-2)
+                    tv = fmod(tmpv, img.height());
+                else tv = tmpv;
                 break;
             case MIRROR:
-                tu = tmpu - (int)tmpu;
-                tv = tmpv - (int)tmpv;
-                lu = (int)tmpu;
-                lv = (int)tmpv;
-                if(lu %2==1)
-                   tu = 1 - tu;
-                if(lv %2==1)
-                   tv = 1 - tv;
+                if(tmpu<0.0f) 
+                    tu = img.width()-1 + fmod(tmpu, img.width());
+                else if(tmpu>img.width()-1)
+                    tu = fmod(tmpu, img.width())-1;
+                else tu = tmpu;
+
+                if(tmpv<0.f) 
+                    tv = img.height()-1 + fmod(tmpv, img.height());
+                else if(tmpv>img.height()-1)
+                    //tv = img.height() - fmod(tmpv, img.height());
+                    tv = fmod(tmpv, img.height())-1;
+                else tv = tmpv;
+                if (!(tu>=0 && (int)tu<=510 && tv>=0 && (int)tv<=510)){
+                    //std::cout<<"Issues";
+                }
+                lu = tmpu / img.width();
+                if(tmpu < 0)
+                    lu = lu + 1;
+                lv = tmpv / img.height();
+                if(tmpv < 0)
+                    lv = lv + 1;
+                if(lu%2==1)
+                    tu = img.width()-1 - tu;
+                if(lv%2==1)
+                    tv = img.height()-1 - tv;
+                //assert(((int)tu)<=510 && ((int)tv)<=510);
                 break;
             }
-            u = tu * (img.width()-1);
-            v = tv * (img.height()-1);
-            lu = min((int)u, (int)img.width()-2);
-            lv = min((int)v, (int)img.height()-2);
-            /*
-            fu = u - (int)u;
-            fv = v - (int)v;
-            if(u>510 ||v>510 )
-                std::cout<<"no u: " <<u << " v: "<<v;
-            return img(1,1);
-            //return lerp2d(img(u,v), img(u,(int)v+1), img((int)u+1, v), img((int)u+1, (int)v+1), fv, fu);
-            */
-            fu = u - lu;
-            fv = v - lv;
-            //if(lu>510 ||lv>510 )
-               // std::cout<<"no u: " <<u << " v: "<<v;
-            //return img(1,1);
-            return lerp2d(img(lu,lv), img(lu,lv+1), img(lu+1, v), img(lu+1, lv+1), fv, fu);
+                 
+            if(tu>510)
+                tu = 510;
+            if(tv>510)
+                tv=510;
+            if(tu<0)
+                tu=0;
+            if(tv<0)
+                tv=0;
+            fu = tu - (int)tu;
+            fv = tv - (int)tv;
+            //assert(((int)tu)<=509 && ((int)tv)<=509);
+            uint u = floor(tu);
+            uint v = floor(tv);
+            return lerp2d(img(u,v), img(u,v+1), img(u+1, v), img(u+1, v+1), fv, fu);
+            //if(v==389)
+            //std::cout<<"Wait";
+            //return img(u,v);
     
     }
     
